182e0067c80edf5cdeeabeaf0fb4509d12eefcf2
Florin <florin.gal12@gmail.com>
2018-03-05 02:00:37 +0200

    check if song is liked already

--- app/helpers.py
+++ app/helpers.py
@@ -1,15 +1,24 @@
+from app.models import Songs
 def parse_response(response):
   video_list = []
   for video in response[1]:
-      _id = video['id']['videoId']
+      _link = "https://www.youtube.com/watch?v=" + video['id']['videoId']
       _title = video['snippet']['title']
       _date = video['snippet']['publishedAt'].split('T')[0]
       _thumbnail = video['snippet']['thumbnails']['default']['url']
-      video_list.append({'id':_id,
+      _liked = check_liked(_link)
+      video_list.append({'link':_link,
         'title':_title,
         'date':_date,
-        'thumbnail':_thumbnail})
+        'thumbnail':_thumbnail,
+        'liked':_liked})
 
   print("Parsed a total of " + str(len(video_list)) + " videos")
   #print(video_list)
   return video_list
+
+def check_liked(link):
+  if Songs.query.filter_by(song_link=link).first():
+    return 'Liked'
+  else:
+    return ''
