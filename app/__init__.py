fe45ff3fafb8d38a36f31b10d447e0bb33e518db
Florin <florin.gal12@gmail.com>
2018-03-27 00:33:34 +0300

    secret key change

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #Load configurations from config file
 app.config.from_object('config')
 db = SQLAlchemy(app)
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.secret_key = app.config['SECRET_KEY']
 
 from app import routes
 print(app.config['SQLALCHEMY_DATABASE_URI'])
